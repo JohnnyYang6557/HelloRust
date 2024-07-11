c8d265eecc6e94359e8c94c44a3cb1af6791da5b
DaMengYang <ycyg6557@outlook.com>
2024-07-11 11:56:15 +0800

    hello_world

---
+++ hello_world/target/debug/deps/hello_world-887edbaefa0f930f.d
@@ -0,0 +1,5 @@
+C:\Users\hello\Documents\vscode\hello_world\target\debug\deps\libhello_world-887edbaefa0f930f.rmeta: src/main.rs
+
+C:\Users\hello\Documents\vscode\hello_world\target\debug\deps\hello_world-887edbaefa0f930f.d: src/main.rs
+
+src/main.rs:
